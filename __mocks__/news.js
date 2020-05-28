6ae1a2a180b30602014bf5aa8b7261df2420d04f
Yasminn Vaz <yasminnvaz@thoughtworks.com>
2020-05-28 20:34:27 -0300

    adding tests to News and Header component

---
+++ __mocks__/news.js
@@ -0,0 +1,181 @@
+module.exports = {
+  status: 'OK',
+  copyright:
+    'Copyright (c) 2020 The New York Times Company. All Rights Reserved.',
+  section: 'World News',
+  last_updated: '2020-05-30T07:38:03-04:00',
+  num_results: 38,
+  results: [
+    {
+      section: 'world',
+      subsection: '',
+      title:
+        'Coronavirus Live Updates: Major U.S. Cities Move to Reopen, but New Outbreaks Persist',
+      abstract:
+        'Businesses are starting up again, but workers who use public transportation to get there face a dilemma. And the E.U. navigates a tangled web of restrictions easing among its member countries.',
+      url:
+        'https://www.nytimes.com/2020/05/30/world/coronavirus-news-update.html',
+      uri: 'nyt://article/7d746c86-b8de-5817-9201-cc8859f71e7d',
+      byline: '',
+      item_type: 'Article',
+      updated_date: '2020-05-30T16:35:27-04:00',
+      created_date: '2020-05-30T00:01:35-04:00',
+      published_date: '2020-05-30T00:01:35-04:00',
+      material_type_facet: '',
+      kicker: '',
+      des_facet: ['Coronavirus (2019-nCoV)'],
+      org_facet: [],
+      per_facet: [],
+      geo_facet: [],
+      multimedia: [
+        {
+          url:
+            'https://static01.nyt.com/images/2020/05/31/us/politics/30virus-wkd-briefing-church/merlin_170520402_b9c7ce69-5b2d-4627-8957-1a866d9d9b06-superJumbo.jpg',
+          format: 'superJumbo',
+          height: 1365,
+          width: 2048,
+          type: 'image',
+          subtype: 'photo',
+          caption:
+            'Volunteers disinfecting a church in March in Mendota, Calif.',
+          copyright: 'Max Whittaker for The New York Times',
+        },
+        {
+          url:
+            'https://static01.nyt.com/images/2020/05/31/us/politics/30virus-wkd-briefing-church/00dc-virus-scotus-thumbStandard.jpg',
+          format: 'Standard Thumbnail',
+          height: 75,
+          width: 75,
+          type: 'image',
+          subtype: 'photo',
+          caption:
+            'Volunteers disinfecting a church in March in Mendota, Calif.',
+          copyright: 'Max Whittaker for The New York Times',
+        },
+        {
+          url:
+            'https://static01.nyt.com/images/2020/05/31/us/politics/30virus-wkd-briefing-church/00dc-virus-scotus-thumbLarge.jpg',
+          format: 'thumbLarge',
+          height: 150,
+          width: 150,
+          type: 'image',
+          subtype: 'photo',
+          caption:
+            'Volunteers disinfecting a church in March in Mendota, Calif.',
+          copyright: 'Max Whittaker for The New York Times',
+        },
+        {
+          url:
+            'https://static01.nyt.com/images/2020/05/31/us/politics/30virus-wkd-briefing-church/merlin_170520402_b9c7ce69-5b2d-4627-8957-1a866d9d9b06-mediumThreeByTwo210.jpg',
+          format: 'mediumThreeByTwo210',
+          height: 140,
+          width: 210,
+          type: 'image',
+          subtype: 'photo',
+          caption:
+            'Volunteers disinfecting a church in March in Mendota, Calif.',
+          copyright: 'Max Whittaker for The New York Times',
+        },
+        {
+          url:
+            'https://static01.nyt.com/images/2020/05/31/us/politics/30virus-wkd-briefing-church/merlin_170520402_b9c7ce69-5b2d-4627-8957-1a866d9d9b06-articleInline.jpg',
+          format: 'Normal',
+          height: 127,
+          width: 190,
+          type: 'image',
+          subtype: 'photo',
+          caption:
+            'Volunteers disinfecting a church in March in Mendota, Calif.',
+          copyright: 'Max Whittaker for The New York Times',
+        },
+      ],
+      short_url: 'https://nyti.ms/2AlleEw',
+    },
+    {
+      section: 'world',
+      subsection: 'europe',
+      title:
+        'A Mile-Long Line for Free Food in Geneva, One of World’s Richest Cities',
+      abstract:
+        'The Swiss city is best known for bankers, watchmakers and U.N. officials. But the virus has forced thousands from Geneva’s underclass to line up for hours for food aid.',
+      url:
+        'https://www.nytimes.com/2020/05/30/world/europe/geneva-coronavirus-reopening.html',
+      uri: 'nyt://article/ecbc8090-6531-5df2-af53-22d607873e7e',
+      byline: 'By Patrick Kingsley and Laetitia Vancon',
+      item_type: 'Article',
+      updated_date: '2020-05-30T14:42:56-04:00',
+      created_date: '2020-05-30T05:00:14-04:00',
+      published_date: '2020-05-30T05:00:14-04:00',
+      material_type_facet: '',
+      kicker: 'Europe Dispatch: A Continent Reopens',
+      des_facet: [
+        'Coronavirus (2019-nCoV)',
+        'Coronavirus Reopenings',
+        'Food Banks and Pantries',
+        'Volunteers and Community Service',
+        'Income Inequality',
+      ],
+      org_facet: [],
+      per_facet: [],
+      geo_facet: ['Europe', 'Geneva (Switzerland)'],
+      multimedia: [
+        {
+          url:
+            'https://static01.nyt.com/images/2020/05/31/world/31roadtrip-stadium-dispatch-fader-1/merlin_172840239_15a6430a-f333-4287-af4f-e4fce1951053-superJumbo.jpg',
+          format: 'superJumbo',
+          height: 1365,
+          width: 2048,
+          type: 'image',
+          subtype: 'photo',
+          caption: '',
+          copyright: 'Laetitia Vancon for The New York Times',
+        },
+        {
+          url:
+            'https://static01.nyt.com/images/2020/05/31/world/31roadtrip-stadium-dispatch-fader-1/merlin_172840239_15a6430a-f333-4287-af4f-e4fce1951053-thumbStandard.jpg',
+          format: 'Standard Thumbnail',
+          height: 75,
+          width: 75,
+          type: 'image',
+          subtype: 'photo',
+          caption: '',
+          copyright: 'Laetitia Vancon for The New York Times',
+        },
+        {
+          url:
+            'https://static01.nyt.com/images/2020/05/31/world/31roadtrip-stadium-dispatch-fader-1/merlin_172840239_15a6430a-f333-4287-af4f-e4fce1951053-thumbLarge.jpg',
+          format: 'thumbLarge',
+          height: 150,
+          width: 150,
+          type: 'image',
+          subtype: 'photo',
+          caption: '',
+          copyright: 'Laetitia Vancon for The New York Times',
+        },
+        {
+          url:
+            'https://static01.nyt.com/images/2020/05/31/world/31roadtrip-stadium-dispatch-fader-1/merlin_172840239_15a6430a-f333-4287-af4f-e4fce1951053-mediumThreeByTwo210.jpg',
+          format: 'mediumThreeByTwo210',
+          height: 140,
+          width: 210,
+          type: 'image',
+          subtype: 'photo',
+          caption: '',
+          copyright: 'Laetitia Vancon for The New York Times',
+        },
+        {
+          url:
+            'https://static01.nyt.com/images/2020/05/31/world/31roadtrip-stadium-dispatch-fader-1/merlin_172840239_15a6430a-f333-4287-af4f-e4fce1951053-articleInline.jpg',
+          format: 'Normal',
+          height: 127,
+          width: 190,
+          type: 'image',
+          subtype: 'photo',
+          caption: '',
+          copyright: 'Laetitia Vancon for The New York Times',
+        },
+      ],
+      short_url: 'https://nyti.ms/2Xegqd1',
+    },
+  ],
+};
--- __tests__/News.test.js
+++ __tests__/News.test.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
 import News from '../src/components/News';
-import MOCK from '../__mocks__/NewsMock.json';
+import MOCK from '../__mocks__/news.js';
 
+const news = MOCK.results[0];
 const author = 'By Natalie Kitroeff and Paulina Villegas';
 const withAuthor = {
-  ...MOCK,
+  ...news,
   byline: author,
 };
 
 describe('<News/>', () => {
   it('should render a title', () => {
-    const {getAllByTestId} = render(<News value={MOCK} />);
+    const {getAllByTestId} = render(<News value={news} />);
     const title = getAllByTestId('title-news')[0].children.toString();
-    expect(title).toEqual(MOCK.title);
+    expect(title).toEqual(news.title);
   });
 
   it('should render an abstract', () => {
-    const {getAllByTestId} = render(<News value={MOCK} />);
+    const {getAllByTestId} = render(<News value={news} />);
     const abstract = getAllByTestId('abstract-news')[0].children.toString();
-    expect(abstract).toEqual(MOCK.abstract);
+    expect(abstract).toEqual(news.abstract);
   });
 
-  it('should render "Live Update" ', () => {
-    const {getAllByTestId} = render(<News value={MOCK} />);
+  it("should render 'Live Update' when don't have news source", () => {
+    const {getAllByTestId} = render(<News value={news} />);
     const liveUpdate = getAllByTestId('live-news')[0];
     expect(liveUpdate).toBeTruthy();
   });
 
-  it('dont should render source ', () => {
+  it('should render source', () => {
     const {getAllByTestId} = render(<News value={withAuthor} />);
-    const author = getAllByTestId('source-news')[0].children.toString();
-    expect(author).toEqual(withAuthor.byline);
+    const source = getAllByTestId('source-news')[0].children.toString();
+    expect(source).toEqual(withAuthor.byline);
   });
 });
